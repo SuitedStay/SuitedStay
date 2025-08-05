50258fb731417708cbbd81e69071ac9e928b5351
James Goodman <james@funderlytics.com>
2025-08-05 17:32:38 +0100

    Add domain redirects for canonical URL

--- next.config.js
+++ next.config.js
@@ -1,4 +1,33 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'www.suitedstay.com' }],
+        destination: 'https://suitedstay.com/:path*',
+        permanent: true,
+      },
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'suitedstays.com' }],
+        destination: 'https://suitedstay.com/:path*',
+        permanent: true,
+      },
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'suitedstay.co.uk' }],
+        destination: 'https://suitedstay.com/:path*',
+        permanent: true,
+      },
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'suitedstays.co.uk' }],
+        destination: 'https://suitedstay.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
